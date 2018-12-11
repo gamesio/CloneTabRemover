b4580592ad8b6db493ebfc6166e9ab45af74a9e4
gamesio <38161971+gamesio@users.noreply.github.com>
2018-12-11 15:30:55 +0100

    Update Remover.js
    Minor bug fixed.

--- Chrome Extension/Remover.js
+++ Chrome Extension/Remover.js
@@ -6,7 +6,10 @@ chrome.history.onVisited.addListener(function(pageOpenedHistory)
 				for(var i = 0; i < openedTabs.length; i++)
 				{
 					if(openedTabs[i].status == "loading")
+					{
 						chrome.tabs.remove(openedTabs[i].id);
+						break;
+					}
 					else
 						chrome.tabs.update(openedTabs[i].id, {"active": true});
 				}
